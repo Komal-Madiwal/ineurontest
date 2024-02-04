0cb7b258ae5271067899a52de0108c3a1e386e80
Komal Madiwal <komalmadiwal10@gmail.com>
2024-02-04 09:50:08 +0530

    adding Q2.py file

--- q1.py
+++ q1.py
@@ -22,7 +22,7 @@ def search_matrix(matrix, target):
 
 # Example usage:
 matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
-target = 50
+target = 3
 
 result = search_matrix(matrix, target)
 print(result)  # Output: True
